35028c99ee42fce4899c101ffe60347334a22e7c
thesarpex <thesarpex@gmail.com>
2025-07-24 13:23:31 +0000

    recreated Login route for student and added it in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Navbar from "./components/student/Navbar";
 import { Route, Routes } from 'react-router-dom';
 import Home from "./routes/student/Home";
 import Signup from "./routes/student/Signup";
-import StudentDashboard from ".routes/student/StudentDashboard";
+import Login from './routes/student/Login';
+import StudentDashboard from "./routes/student/StudentDashboard";
 import StudentAllCourses from "./routes/student/StudentAllCourses";
-import Player from ".routes/student/Player";
-import Settings from ".routes/student/Settings";
+import Player from "./routes/student/Player";
+import Settings from "./routes/student/Settings";
 
 function App() {
   const {loading, currentUser} = useAuth();
@@ -25,7 +26,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
-        
+        <Route path='/login' element={<Login />} />
         <Route path='/student-dashboard' element={<StudentDashboard />} />
         <Route path='/student-all-courses' element={<StudentAllCourses />} />
         <Route path='/player' element={<Player />} />
