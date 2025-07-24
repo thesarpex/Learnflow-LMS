6e91fef37bd75c06d10dd67f512a1db8c241da85
thesarpex <thesarpex@gmail.com>
2025-07-24 12:55:50 +0000

    modified app.jsx, deleted and recreated login route for student, addedroutes in app.jsx for student

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { useAuth } from './context/AuthContext';
+import Navbar from "./components/student/Navbar";
+import { Route, Routes } from 'react-router-dom';
+import Home from "./routes/student/Home";
+import Signup from "./routes/student/Signup";
+import StudentDashboard from ".routes/student/StudentDashboard";
+import StudentAllCourses from "./routes/student/StudentAllCourses";
+import Player from ".routes/student/Player";
+import Settings from ".routes/student/Settings";
 
-const App = () => {
+function App() {
+  const {loading, currentUser} = useAuth();
+
+  if(loading) {
+      return (
+    <div>Loading...</div>
+  );
+  }
+
   return (
-    <div>App</div>
+    <>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        
+        <Route path='/student-dashboard' element={<StudentDashboard />} />
+        <Route path='/student-all-courses' element={<StudentAllCourses />} />
+        <Route path='/player' element={<Player />} />
+        <Route path='settings' element={<Settings />} />
+      </Routes>
+    </div>
+    </>
   )
+
 }
 
 export default App
