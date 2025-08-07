f0077c09211cfd8a85ce65aa8cdec2f224f9857f
thesarpex <thesarpex@gmail.com>
2025-08-07 12:18:04 +0000

    styled password and confirm password input on sign up page

--- src/routes/student/Signup.jsx
+++ src/routes/student/Signup.jsx
@@ -41,106 +41,108 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <div className="flex flex-col gap-4 border border-gray-400 w-fit rounded-lg shadow-xl p-4">
-        <div>
-          <p className="text-2xl">Welcome!</p>
-          <h2 className="text-3xl font-medium">
-            {""}Sign up to
-          </h2>
-          <p className="text-xl">create your account</p>
-        </div>
-
-        {error && (
-          <div className="bg-red-50 text-red-700 p-3 rounded-md mb-4 text-sm">
-            {error}
-            {""}
-          </div>
-        )}
-
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+    <div className="max-w-md mx-auto mt-15">
+      <div className="flex flex-col gap-4 border border-gray-400 w-fit rounded-lg shadow-xl p-10">
           <div>
-            <div>I am signing up as :</div>
-            <div className="flex gap-4">
-              <div>
-                <label className="pr-1">Student</label>
-                <input
-                  id="student-radio"
-                  type="radio"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
+            <p className="text-2xl">Welcome!</p>
+            <h2 className="text-3xl font-medium">{""}Sign up to</h2>
+            <p className="text-xl">create your account</p>
+          </div>
 
-              <div>
-                <label className="pr-1">Tutor</label>
-                <input
-                  id="tutor-radio"
-                  type="radio"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
+          {error && (
+            <div className="bg-red-50 text-red-700 p-3 rounded-md mb-4 text-sm">
+              {error}
+              {""}
+            </div>
+          )}
+
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div>
+              <div>I am signing up as :</div>
+              <div className="flex gap-4">
+                <div>
+                  <label className="pr-1">Student</label>
+                  <input
+                    id="student-radio"
+                    type="radio"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label className="pr-1">Tutor</label>
+                  <input
+                    id="tutor-radio"
+                    type="radio"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full block border border-gray-400 rounded-sm text-gray-500 px-3 py-2 focus:outline-none focus:ring-1"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-100 block border border-gray-400 rounded-sm text-gray-500 px-3 py-2 focus:outline-none focus:ring-1 mt-1"
+                placeholder="Enter your email"
+                required
+              />
+            </div>
 
-          {/*<div>
+            {/*<div>
           <label htmlFor='username'>
             User name
           </label>
           <input id='username' type='text' onChange={''} placeholder='Enter your user name' required />
         </div>*/}
 
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter your password"
-              required
-            />
+            <div className="flex flex-col gap-4">
+              <div>
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter your password"
+                  className="w-full block border border-gray-400 rounded-sm text-gray-500 px-3 py-2 focus:outline-none focus:ring-1 mt-1"
+                  required
+                />
+              </div>
 
-            <div>
-              <label htmlFor="password-confirm">Confirm Password</label>
-              <input
-                id="password-confirm"
-                type="password"
-                value={passwordConfirm}
-                onChange={(e) => setPasswordConfirmed(e.target.value)}
-                placeholder="Confirm your password"
-                required
-              />
+              <div>
+                <label htmlFor="password-confirm">Confirm Password</label>
+                <input
+                  id="password-confirm"
+                  type="password"
+                  value={passwordConfirm}
+                  onChange={(e) => setPasswordConfirmed(e.target.value)}
+                  className="w-full block border border-gray-400 rounded-sm text-gray-500 px-3 py-2 focus:outline-none focus:ring-1 mt-1"
+                  placeholder="Confirm your password"
+                  required
+                />
+              </div>
             </div>
+
+            <button type="submit" disabled={loading}>
+              {loading ? "Creating Account..." : "Register"}
+            </button>
+          </form>
+
+          <div className="flex flex-col items-center">
+            <p>
+              Already have an account?
+              <Link to="/login">Login</Link>
+            </p>
           </div>
-
-          <button type="submit" disabled={loading}>
-            {loading ? "Creating Account..." : "Register"}
-          </button>
-        </form>
-
-        <div>
-          <p>
-            Already have an account?
-            <Link to="/login">Login</Link>
-          </p>
-        </div>
       </div>
     </div>
   );
