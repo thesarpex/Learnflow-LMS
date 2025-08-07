36aec412c93984ea455b6d7a725818ea0b720449
thesarpex <thesarpex@gmail.com>
2025-08-07 22:02:38 +0000

    created functions for login page

--- src/routes/student/Login.jsx
+++ src/routes/student/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    if (!email || !password) {
+      return setError("Please fill in all fields");
+    }
+
+    try {
+      setLoading(true);
+      await login(email, password);
+      navigate("/student-dashboard");
+    } catch (err) {
+      setError(
+        "Failed to access account: " + (err.message || "Please try again")
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div></div>
+  );
+};
+
+export default Login;
