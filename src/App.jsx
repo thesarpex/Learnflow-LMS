8b6d3bbea82ef2d56a995df4431c2d8b1fab2aa6
thesarpex <thesarpex@gmail.com>
2025-08-09 12:30:27 +0000

    set tutor dashboard as outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,65 @@
-import React from 'react'
-import { useAuth } from './context/AuthContext';
+import React from "react";
+import { useAuth } from "./context/AuthContext";
 import Navbar from "./components/student/Navbar";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/student/Home";
 import Signup from "./routes/student/Signup";
-import Login from './routes/student/Login';
+import Login from "./routes/student/Login";
 import StudentDashboard from "./routes/student/StudentDashboard";
 import StudentAllCourses from "./routes/student/StudentAllCourses";
 import Player from "./routes/student/Player";
 import Settings from "./routes/student/Settings";
-import Tutor from './routes/tutor/Tutor';
+import Tutor from "./routes/tutor/Tutor";
 import TutorDashboard from "./routes/tutor/TutorDashboard";
 import CourseBuilder from "./routes/tutor/CourseBuilder";
 import TutorAllCourses from "./routes/tutor/TutorAllCourses";
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const {loading, currentUser} = useAuth();
+  const { loading, currentUser } = useAuth();
 
-  if(loading) {
-      return (
-    <div>Loading...</div>
-  );
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
   return (
     <>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path='/student-dashboard' element={
-          <ProtectedRoute>
-            <StudentDashboard />
-          </ProtectedRoute>
-        }>
-          <Route path='student-all-courses' element={<StudentAllCourses />} />
-        <Route path='player' element={<Player />} />
-        <Route path='settings' element={<Settings />} />
-        </Route>
+          <Route
+            path="/student-dashboard"
+            element={
+              <ProtectedRoute>
+                <StudentDashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="all-courses" element={<StudentAllCourses />} />
+            <Route path="player" element={<Player />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
 
-        
-
-        <Route path='/tutor' element={<Tutor />}>
-            
-            <Route path='tutor-dashboard' element={
+          <Route
+            path="tutor-dashboard"
+            element={
               <ProtectedRoute>
                 <TutorDashboard />
               </ProtectedRoute>
-            } />
-            
-            <Route path='course-builder' element={<CourseBuilder />} />
-            <Route path='all-courses' element={<TutorAllCourses />} />
-            <Route path='settings' element={<Settings />} />
-        </Route>
-      </Routes>
-    </div>
+            }
+          >
+            <Route path="course-builder" element={<CourseBuilder />} />
+            <Route path="all-courses" element={<TutorAllCourses />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+        </Routes>
+      </div>
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
--- src/routes/tutor/TutorDashboard.jsx
+++ src/routes/tutor/TutorDashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 const TutorDashboard = () => {
   return (
     <div>
       <div>TutorDashboard</div>
+      <div>{<Outlet/>}</div>
     </div>
     
   )
